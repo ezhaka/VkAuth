55f33a7c3fcd803aebbeb5e18226560019521823
mcgee <cookies.moonster@gmail.com>
2013-12-20 20:37:38 +0400

    first commit

---
+++ ConsoleApplication2/ConsoleApplication2/CookieAwareWebClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApplication2
+{
+    public class CookieAwareWebClient : WebClient
+    {
+        public CookieContainer CookieContainer { get; set; }
+        public Uri ResponseUri { get; private set; }
+
+        public CookieAwareWebClient()
+        {
+            CookieContainer = new CookieContainer();
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+
+            HttpWebRequest webRequest = request as HttpWebRequest;
+            if (webRequest != null)
+            {
+                webRequest.CookieContainer = CookieContainer;
+            }
+
+            return request;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            WebResponse response = base.GetWebResponse(request);
+            ResponseUri = response.ResponseUri;
+            return response;
+        }
+    }
+}
