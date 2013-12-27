4c7305815b63f3373c81709bd5f549b5eb9bb3e3
mcgee <cookies.moonster@gmail.com>
2013-12-27 11:55:19 +0400

    security check

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Web;
-using HtmlAgilityPack;
 
 namespace ConsoleApplication2
 {
@@ -12,100 +7,23 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            var client = new CookieAwareWebClient();
+            VkApiClient vkApiClient = new VkApiClient(
+                "4070029",
+                "groups",
+                "5.5",
+                "+79164556832",
+                "SslDemo:)");
 
-            client.Encoding = Encoding.UTF8;
+            NameValueCollection parameters = new NameValueCollection();
+            parameters.Add("gid", "30621569");
+            parameters.Add("tid", "27884279");
+            parameters.Add("extended", "1");
+            parameters.Add("offset", "0");
+            parameters.Add("count", "100");
 
-            string appId = "4070029";
-            string permissions = "groups";
-            string redirectUri = "";
-            string display = "page";
-            string apiVersion = "5.5";
+            string jsonBoardMessages = vkApiClient.ExecuteMethod("board.getComments", parameters);
 
-            string authUrl = string.Format("https://oauth.vk.com/authorize?client_id={0}&scope={1}&redirect_uri={2}&display={3}&v={4}&response_type=token", appId, permissions, HttpUtility.UrlEncode(redirectUri), display, HttpUtility.UrlEncode(apiVersion));
-
-            string downloadString = client.DownloadString(authUrl);
-
-            HtmlDocument htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(downloadString);
-
-            HtmlNode form = htmlDocument.DocumentNode.SelectSingleNode("//form");
-            string postUrl = GetHtmlAttributeIgnoreCase(form, "action");
-
-            HtmlNodeCollection inputs = form.SelectNodes("//input");
-            NameValueCollection requestParams = new NameValueCollection();
-
-            foreach (var inputNode in inputs)
-            {
-                var type = GetHtmlAttributeIgnoreCase(inputNode, "type");
-
-                if (type == "submit")
-                {
-                    continue;
-                }
-
-                var name = GetHtmlAttributeIgnoreCase(inputNode, "name");
-
-                if (name == "email")
-                {
-                    requestParams.Add(name, "+79164556832");
-                }
-                else if (name == "pass")
-                {
-                    requestParams.Add(name, "SslDemo:)");
-                }
-                else
-                {
-                    requestParams.Add(name, GetHtmlAttributeIgnoreCase(inputNode, "value"));
-                }
-            }
-
-            requestParams.Add("expire", "0");
-
-            byte[] responsebytes = client.UploadValues(postUrl, "POST", requestParams);
-            string responsebody = Encoding.UTF8.GetString(responsebytes);
-
-
-            Uri responseUri = client.ResponseUri;
-
-            NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(responseUri.ToString().Split('#')[1]);
-            string accessToken = nameValueCollection["access_token"];
-
-            NameValueCollection boardParams = new NameValueCollection();
-            boardParams.Add("gid", "30621569");
-            boardParams.Add("tid", "27884279");
-            boardParams.Add("extended", "0");
-            boardParams.Add("offset", "0");
-            boardParams.Add("count", "100");
-            boardParams.Add("access_token", accessToken);
-
-
-            string getCommentsUrl = string.Format("https://api.vk.com/method/{0}{1}", "board.getComments", ToQueryString(boardParams));
-            string boardCommentsJson = client.DownloadString(getCommentsUrl);
-
-            // auth_token........
-
-            Console.WriteLine(responsebody);
-        }
-
-        private static string GetHtmlAttributeIgnoreCase(HtmlNode inputNode, string attrName)
-        {
-            string name = inputNode.Attributes.First(a => a.Name.Equals(attrName, StringComparison.InvariantCultureIgnoreCase)).Value;
-            return name;
-        }
-
-        private static string ToQueryString(NameValueCollection nameValueCollection)
-        {
-            List<string> result = new List<string>();
-
-            foreach (string key in nameValueCollection.AllKeys)
-            {
-                string value = nameValueCollection[key];
-                string pair = string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(value));
-                result.Add(pair);
-            }
-
-            return "?" + string.Join("&", result);
+            Console.WriteLine(jsonBoardMessages);
         }
     }
 }
---
+++ ConsoleApplication2/ConsoleApplication2/VkApiClient.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace ConsoleApplication2
+{
+    public class VkApiClient
+    {
+        private readonly string appId;
+        private readonly string permissions;
+        private readonly string apiVersion;
+        private readonly string phoneNumber;
+        private readonly string password;
+        private const string redirectUri = "";
+        private const string display = "page";
+        private readonly CookieAwareWebClient webClient;
+
+        private string accessToken;
+
+        public VkApiClient(string appId, string permissions, string apiVersion, string phoneNumber, string password)
+        {
+            this.appId = appId;
+            this.permissions = permissions;
+            this.apiVersion = apiVersion;
+            this.phoneNumber = phoneNumber;
+            this.password = password;
+
+            this.webClient = new CookieAwareWebClient();
+            webClient.Encoding = Encoding.UTF8;
+        }
+
+        public string ExecuteMethod(string methodName, NameValueCollection parameters)
+        {
+            if (string.IsNullOrEmpty(this.accessToken))
+            {
+                this.Authorize();
+            }
+
+            parameters.Add("access_token", this.accessToken);
+            string url = string.Format("https://api.vk.com/method/{0}{1}", methodName, ToQueryString(parameters));
+
+            return webClient.DownloadString(url);
+        }
+
+        private void Authorize()
+        {
+            string authUrl = string.Format(
+                "https://oauth.vk.com/authorize?client_id={0}&scope={1}&redirect_uri={2}&display={3}&v={4}&response_type=token",
+                HttpUtility.UrlEncode(appId),
+                HttpUtility.UrlEncode(permissions),
+                HttpUtility.UrlEncode(redirectUri),
+                HttpUtility.UrlEncode(display),
+                HttpUtility.UrlEncode(apiVersion));
+
+            string responseContent = webClient.DownloadString(authUrl);
+
+            Tuple<string, NameValueCollection> formData = this.ParseForm(responseContent);
+            string postUrl = formData.Item1;
+            NameValueCollection requestParams = formData.Item2;
+
+            requestParams["email"] = this.phoneNumber;
+            requestParams["pass"] = this.password;
+            requestParams.Add("expire", "0");
+
+            byte[] credentialsResponse = webClient.UploadValues(postUrl, "POST", requestParams);
+            string credentialsResponseContent = webClient.Encoding.GetString(credentialsResponse);
+
+            if (webClient.ResponseUri.ToString().Contains("security_check"))
+            {
+                this.DoSecurityCheck(credentialsResponseContent);
+            }
+
+            string responseUrl = webClient.ResponseUri.ToString();
+            NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(responseUrl.Split('#')[1]);
+            this.accessToken = nameValueCollection["access_token"];
+        }
+
+        private void DoSecurityCheck(string responseContent)
+        {
+            Tuple<string, NameValueCollection> formData = this.ParseForm(responseContent);
+            string postUrl = formData.Item1;
+            NameValueCollection requestParams = formData.Item2;
+
+            Regex cleanRegex = new Regex(@"\D");
+            string phoneNumbers = cleanRegex.Replace(this.phoneNumber, "");
+
+            requestParams["code"] = phoneNumbers.Substring(1, 8);
+
+            webClient.UploadValues(postUrl, "POST", requestParams);
+        }
+
+        private Tuple<string, NameValueCollection> ParseForm(string downloadString)
+        {
+            NameValueCollection requestParams = new NameValueCollection();
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(downloadString);
+
+            HtmlNode form = htmlDocument.DocumentNode.SelectSingleNode("//form");
+            string postUrl = GetHtmlAttributeIgnoreCase(form, "action");
+
+            HtmlNodeCollection inputs = form.SelectNodes("//input");
+            requestParams = new NameValueCollection();
+
+            foreach (var inputNode in inputs)
+            {
+                var type = GetHtmlAttributeIgnoreCase(inputNode, "type");
+
+                if (type == "submit")
+                {
+                    continue;
+                }
+
+                var name = GetHtmlAttributeIgnoreCase(inputNode, "name");
+                requestParams.Add(name, this.GetHtmlAttributeIgnoreCase(inputNode, "value"));
+            }
+
+            return new Tuple<string, NameValueCollection>(postUrl, requestParams);
+        }
+
+        private string GetHtmlAttributeIgnoreCase(HtmlNode inputNode, string attrName)
+        {
+            HtmlAttribute attribute = inputNode.Attributes.FirstOrDefault(a => a.Name.Equals(attrName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (attribute == null)
+            {
+                return string.Empty;
+            }
+
+            return attribute.Value;
+        }
+
+        private string ToQueryString(NameValueCollection nameValueCollection)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string key in nameValueCollection.AllKeys)
+            {
+                string value = nameValueCollection[key];
+                string pair = string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(value));
+                result.Add(pair);
+            }
+
+            return "?" + string.Join("&", result);
+        }
+    }
+}
